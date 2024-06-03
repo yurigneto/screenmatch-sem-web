a76add0be644ec6c6e6574f5578c6396cdc21ea9
yurigneto <yuri.g.neto6@hotmail.com>
2024-06-03 10:59:33 -0300

    Implementa a funcionalidade de buscar o primeiro episódio por um trecho do título. Implementa a reprodução das estátisticas da série.

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -9,10 +9,7 @@ import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -52,11 +49,16 @@ public class Principal {
         List<DadosEpisodio> dadosEpisodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream())
                 .collect(Collectors.toList());
-        System.out.println("\nTop 5 episódios");
+        System.out.println("\nTop 10 episódios");
         dadosEpisodios.stream()
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .filter(e -> !e.avaliacao().equalsIgnoreCase(" N/A "))
+                .peek(e -> System.out.println(" Primeiro filtro (N/A) " + e))
                 .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
+                .peek(e -> System.out.println(" Ordenação " + e))
+                .limit(10)
+                .peek(e -> System.out.println(" Limite " + e))
+                .map(e-> e.titulo().toUpperCase())
+                .peek(e -> System.out.println(" Mapeamento " + e))
                 .forEach(System.out::println);
 
         List<Episodio> episodios = temporadas.stream()
@@ -65,6 +67,19 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+
+        System.out.println("Digite um trecho do título do episódio: ");
+        var trechoTitulo = leitura.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if(episodioBuscado.isPresent()){
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+        }else{
+            System.out.println("Episódio não encontrado!");
+        }
 
 
         System.out.println("A partir de que ano você deseja ver os episódios? ");
@@ -81,5 +96,21 @@ public class Principal {
                         "Episódio: " + e.getTitulo() +
                         "Data lançamento: " + e.getDataLancamento().format(formatador)
         ));
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
+
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio:  " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
+
     }
 }
